16ade49f8b34833cff81d67964e9e5b7d4af4d0c
Abdullah Zayed Al-Muhaisen <aalmuhaisen@transition-se.com>
2025-01-31 18:26:07 +0300

    commit=>carseerApp

---
+++ CarProjectApp/Controller/VehicleController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VehicleController : ControllerBase
+{
+    private readonly HttpClient _httpClient;
+
+    public VehicleController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    [HttpGet("getAllMakes")]
+    public async Task<IActionResult> GetAllMakes()
+    {
+        try
+        {
+            var response = await _httpClient.GetStringAsync("https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json");
+            var result = JsonSerializer.Deserialize<Makes>(response);
+            return Ok(response);
+        }
+        catch (Exception ex) {
+            throw;
+        
+        }
+      
+    }
+
+    [HttpGet("getVehicleTypes/{makeId}")]
+    public async Task<IActionResult> GetVehicleTypes(int makeId)
+    {
+        var response = await _httpClient.GetStringAsync($"https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMakeId/{makeId}?format=json");
+        var result = JsonSerializer.Deserialize<Makes>(response);
+        return Ok(result.Results);
+    }
+
+    [HttpGet("getModels/{makeId}/{year}")]
+    public async Task<IActionResult> GetModels(int makeId, int year)
+    {
+        try
+        {
+            var response = await _httpClient.GetStringAsync($"https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/{makeId}/modelyear/{year}?format=json");
+        var result = JsonSerializer.Deserialize<Makes>(response);
+        return Ok(result.Results);
+        }
+        catch (Exception ex)
+        {
+            throw;
+
+        }
+    }
+
+
+    public class Makes
+    {
+        [JsonPropertyName("Results")]
+        public List<Vehicle> Results { get; set; }
+
+    }
+      
+    public class Vehicle
+    {
+        [JsonPropertyName("Make_ID")]
+        public int MakeId { get; set; }
+
+        [JsonPropertyName("Make_Name")]
+        public string MakeName { get; set; } 
+        
+        [JsonPropertyName("Model_Name")]
+        public string ModelName { get; set; }  
+        
+        [JsonPropertyName("Model_ID")]
+        public int ModelID { get; set; } 
+ 
+        [JsonPropertyName("VehicleTypeName")]
+        public string VehicleTypeName { get; set; }
+        [JsonPropertyName("VehicleTypeId")]
+        public int VehicleTypeId { get; set; }
+    }
+}
--- CarProjectApp/Program.cs
+++ CarProjectApp/Program.cs
@@ -3,8 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddHttpClient();
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -12,12 +12,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.MapControllers();
 app.UseAuthorization();
 
 app.MapRazorPages();
